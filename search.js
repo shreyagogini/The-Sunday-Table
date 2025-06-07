7110866a221a199ae82d1a6c444df6b76ab546df
Shreya Gogini <shreyagogini@gmail.com>
2025-06-07 15:09:57 -0700

    recipes+search

--- search.js
+++ search.js
@@ -1,12 +1,19 @@
 const recipes = [
     { name: "Jasmine Rice", url: "jasminericeR.html" },
-    { name: "Mango Rice", url: "mangoriceR.html" },
-    { name: "Chocolate Cookies", url: "chococookiesR.html" },
+    { name: "Mango Sticky Rice", url: "mangoriceR.html" },
+    { name: "Double Chocolate Cookies", url: "chococookiesR.html" },
     { name: "Pumpkin Pie", url: "pumpkinpieR" },
     { name: "Chicken", url: "chickenR.html" },
     { name: "Teriyaki Chicken", url: "teriyakichickenR.html" },
     { name: "Vodka Pasta", url: "vodkapastaR.html" },
-    { name: "Spinach Chicken Dumplings", url: "spinachchickendumplingsR.html" },
+    { name: "Chicken Spinach Dumplings", url: "chickenspindumpR.html" },
+    { name: "Sourdough Bread", url: "sourdoughR.html" },
+    { name: "Yogurt Parfait", url: "yogurtparfaitR.html" },
+    { name: "Rotisserie Chicken", url: "chickenR.html" },
+    { name: "Mango Sticky Rice", url: "mangoriceR.html" },
+    { name: "Trail Mix", url: "trailmixR.html" },
+    { name: "Caesar Salad", url: "caesarsaladR.html" },
+    { name: "Pasta al la Vodka", url: "pastalavodkaR.html" },
     // Add more recipes here
 ];
 
